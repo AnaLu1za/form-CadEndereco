08089831e25f3262f0d9442a6340a19cfbab6e75
AnaLu1za <aninha141108@gmail.com>
2024-09-20 07:20:02 -0300

    Adiciona função limparFormulario() que, acessa cada elemento do formulário pelo seu ID (rua, bairro, cidade e estado). Atribui uma sting vazia ao valor de cada elemento, efetivamente limpando os campos.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 'use strict'// ATIVA O MODO RESTRITO
+
 //CODIGO PARA CONSUMO DE API DE VIA CEP
 //https://viacep.com.br/
+
+// LIMPAR CONSULTA DO FORM JÁ REALIZADA
+const limparFormulario = () => {
+    document.getElementById('rua').value= '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
+    document.getElementById('estado').value = '';
+}
+ 
