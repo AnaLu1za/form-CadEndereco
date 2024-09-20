4d2b7cd382877bedeaf3c5729349ba4baf41ce19
AnaLu1za <aninha141108@gmail.com>
2024-09-20 07:42:47 -0300

    A função preencherFormulario tem como objetivo preencher os campos de um formulário HTML com as informações de um endereço obtidas a partir de uma API, no caso, o ViaCEP. A função recebe um parâmetro denominado endereco. Este parâmetro é um objeto JavaScript que contém as informações do endereço retornadas pela API. Para cada propriedade do objeto endereco, a função utiliza o método getElementById para obter uma referência ao elemento HTML correspondente no formulário. Esses elementos HTML possuem IDs que correspondem aos nomes das propriedades do objeto endereco (por exemplo, rua, bairro, cidade, estado). Uma vez obtida a referência ao elemento HTML, a função atribui o valor da propriedade correspondente do objeto endereco ao valor do elemento HTML. Isso é feito utilizando a propriedade value do elemento.

--- script.js
+++ script.js
@@ -17,3 +17,11 @@ const eNumero = (numero) => /^[0-9]+$/.test(numero);
 // VERIFICA O TAMANHO DO CEP
 const cepValido = (cep) => cep.length == 8 &&
 eNumero(cep);
+
+// FUNÇÃO PARA PREENCHER CAMPOS RELACIONADOS AO CEP
+const preencherFormulario = (endereco) => {
+    document.getElementById('rua').value = endereco.logradouro;//COLOCA O VALOR DE LOGRADOURO DA API DENTRO DO CAMPO LOGRADOURO DO FORMULARIO
+    document.getElementById('bairro').value = endereco.bairro
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+}
